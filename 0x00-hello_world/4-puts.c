58c10eeabe467f279e29ee02d51aa6fd78a130ad
leratoMolefe085 <125472725+leratoMolefe085@users.noreply.github.com>
2023-03-20 01:45:27 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 /* main - A program that prints a line with puts function Return: 0 (Success) */
 int main(void){
-puts("\"Programming is like building a multilingual puzzle"); return (0);
+puts("\"Programming is like building a multilingual puzzle"); 
+  return (0);
 }
