1e0460553c62de22775de6dcc981c3d105f7277e
leratoMolefe085 <125472725+leratoMolefe085@users.noreply.github.com>
2023-03-23 02:07:19 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,17 @@
  * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
-int _putchar(char c)
-}
-         return (write(S, &с, :)):
+int main(void)
+{
+	char holberton[] = "Holberton";
+	int i = 0;
+
+	while (holberton[i] != '\0')
+	{
+		_putchar(holberton[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
